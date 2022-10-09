62e585375dff64aa8d4920c48ce51245ae19c670
romashorodok <108732029+romashorodok@users.noreply.github.com>
2022-10-09 17:53:38 +0300

    feat: Added featured post section.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,19 +12,23 @@ type Post = {
   title: string;
   created_at: string;
   description: string;
+
+  // TODO: Add image support
+  image?: string;
 };
 
 interface Props {
   posts: Array<Post>;
   categories: Array<Category>;
+  featuredPost: Post;
 }
 
-export function Home({ posts, categories }: Props) {
+export function Home({ posts, categories, featuredPost }: Props) {
   const [selectedCategory, setSelectedCategory] = React.useState(0);
 
   return (
     <LandingLayout>
-      <div
+      <section
         className={`${Styles.landing_background} flex flex-col justify-center`}
       >
         <div className={`${Styles.landing_text} text-4xl font-bold`}>
@@ -32,9 +36,9 @@ export function Home({ posts, categories }: Props) {
           <p>вам для комфортного</p>
           <p>проводження часу</p>
         </div>
-      </div>
+      </section>
 
-      <div className={`${Styles.landing_recent_posts}`}>
+      <section className={`${Styles.landing_recent_posts}`}>
         <h1 className="text-4xl font-bold mb-10">Нещодавні пости</h1>
 
         <div className="flex flex-row justify-between flex-wrap mb-10 bg-support">
@@ -68,7 +72,24 @@ export function Home({ posts, categories }: Props) {
             </Card>
           ))}
         </div>
-      </div>
+      </section>
+
+      <section
+        className={`${Styles.landing_featured_post} relative`}
+        style={{ backgroundImage: `url(${featuredPost.image})` }}
+      >
+        <div
+          className={`${Styles.landing_featured_post_text} flex flex-col items-center justify-center absolute fg-normal`}
+        >
+          <p className="text-4xl font-bold">{featuredPost.title}</p>
+          <p
+            className={`${Styles.landing_featured_post_description__separator} text-lg`}
+          >
+            {featuredPost.description}
+          </p>
+          <p>{featuredPost.created_at}</p>
+        </div>
+      </section>
     </LandingLayout>
   );
 }
@@ -137,10 +158,19 @@ export function getServerSideProps(): GetServerSidePropsResult<Props> {
     ],
   };
 
+  const serverFeaturedPostResult = {
+    title: "Моя мрія відвідати Париж цього року",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing  elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    created_at: "04.10.2022",
+    image: "/assets/featured_mock.jpg",
+  };
+
   return {
     props: {
       posts: serverPostsResult.data,
       categories: serverCategoriesResult.data,
+      featuredPost: serverFeaturedPostResult,
     },
   };
 }
